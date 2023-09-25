86930300eef1571d1e85c45317d60a46a1fdd191
falba3 <99144557+falba3@users.noreply.github.com>
2023-09-25 15:30:14 +0200

    0. Setup

--- masker.py
+++ masker.py
@@ -1,14 +1,29 @@
 import os
-import graphics
+from PIL import Image as pil_Image
+from graphics import GraphWin, Image, Point, Rectangle
+
 
 def masker(images_directory):
-    images = os.listdir(images_directory)
-    for image in images:
-        print(image)
-
     if not os.path.exists('masks'):
         os.mkdir('masks')
-    else:
-        pass
+
+    os.chdir(images_directory)
+    images = os.listdir()
+    for image_n in images:
+        image = pil_Image.open(image_n)
+        width, height = image.size
+        win = GraphWin(image_n, width, height)
+        img = Image(Point(width/2, height/2), image_n)
+        img.draw(win)
+        top_left = win.getMouse()
+        bottom_right = win.getMouse()
+        box = Rectangle(top_left, bottom_right)
+        box.draw(win)
+        win.getMouse()
+        win.close()
+        top_left = top_left.getX(), top_left.getY()
+        bottom_right = bottom_right.getX(), bottom_right.getY()
+
+
 
 masker('images')
