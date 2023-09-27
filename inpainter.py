2d004255f994d11d6831d841ea2c16bd05cf4508
falba3 <99144557+falba3@users.noreply.github.com>
2023-09-27 13:24:14 +0200

    3. Inpainter

---
+++ inpainter.py
@@ -0,0 +1,24 @@
+import os
+import logging
+
+def inpainter(images_directory):
+    if os.path.exists(images_directory):
+        if os.path.exists(images_directory + '/masks'):
+            images = os.listdir(images_directory)
+            for image_n in images:
+                if image_n != "masks" and image_n != '.DS_Store' and image_n != 'error.log':
+                    name = image_n[:-4]
+                    if os.path.exists(f"{images_directory}/{name}.png") and os.path.exists(f"{images_directory}/masks/{name}_mask.png"):
+                        print("nuts")
+                    else:
+                        logging.basicConfig(filename='error.log', level=logging.ERROR)
+                        raise Exception(f"missing mask for {name}.png")
+
+        else:
+            logging.basicConfig(filename='error.log', level=logging.ERROR)
+            raise Exception(f"'masks' folder does not exist")
+    else:
+        logging.basicConfig(filename='error.log', level=logging.ERROR)
+        raise Exception(f"directory '{images_directory}' does not exist")
+
+inpainter('images')
