9539d0a433ec7e5b799ca7db1618b3e488b562be
falba3 <99144557+falba3@users.noreply.github.com>
2023-09-27 13:47:31 +0200

    3. Inpainter

--- inpainter.py
+++ inpainter.py
@@ -9,7 +9,19 @@ def inpainter(images_directory):
                 if image_n != "masks" and image_n != '.DS_Store' and image_n != 'error.log':
                     name = image_n[:-4]
                     if os.path.exists(f"{images_directory}/{name}.png") and os.path.exists(f"{images_directory}/masks/{name}_mask.png"):
-                        print("nuts")
+                        print(f"image:  {images_directory}/{name}.png\nmask:    {images_directory}/masks/{name}_mask.png")
+
+                        # image_output = pipe(prompt=prompt,
+                        #                     image=background,
+                        #                     mask_image=mask,
+                        #                     width=background.size[0],
+                        #                     height=background.size[1],
+                        #                     negative_prompt=negative_prompt,
+                        #                     strength=strength,
+                        #                     num_inference_steps=num_inference_steps,
+                        #                     guidance_scale=guidance_scale
+                        #                     ).images[0]
+                        # image_output.save(f'output/{name}_out.png')
                     else:
                         logging.basicConfig(filename='error.log', level=logging.ERROR)
                         raise Exception(f"missing mask for {name}.png")
