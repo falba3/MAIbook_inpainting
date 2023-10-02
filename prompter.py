3996d8603c1a770d76880d172fad9e738489a26e
falba3 <99144557+falba3@users.noreply.github.com>
2023-10-02 15:05:08 +0200

    3. Inpainter and Prompter

--- prompter.py
+++ prompter.py
@@ -10,7 +10,7 @@ def prompter(images_directory):
             os.chdir(images_directory)
             if not os.path.exists('prompts'):
                 os.mkdir('prompts')
-            images = os.listdir('masks')
+            images = os.listdir()
             for image_n in images:
                 if image_n != "masks" and image_n != '.DS_Store' and image_n != 'error.log' and image_n != 'prompts':
                     image = pil_Image.open(image_n)
@@ -18,7 +18,9 @@ def prompter(images_directory):
                     win = GraphWin(image_n, width, height)
                     img = Image(Point(width/2, height/2), image_n)
                     img.draw(win)
-                    win.getMouse()
+                    prompt = input("Prompt: ")
+                    with open(f"prompts/{image_n[:-4]}.txt", 'w') as file:
+                        file.write(prompt)
                     win.close()
         else:
             logging.basicConfig(filename='error.log', level=logging.ERROR)
