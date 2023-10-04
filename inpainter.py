fac607b5e1d4f29e83266232001332400f6e36a1
falba3 <99144557+falba3@users.noreply.github.com>
2023-10-04 14:37:29 +0200

    5. testing: strength=0.99999, AbsoluteRealism

--- inpainter.py
+++ inpainter.py
@@ -33,7 +33,7 @@ def inpainter(images_directory):
                                              width=background.size[0],
                                              height=background.size[1],
                                             negative_prompt=negative_prompt,
-                                            strength=1,
+                                            strength=0.99999,
                                             # strength=1,
                                             num_inference_steps=20,
                                             guidance_scale=10
