699eb07d7d43d845fe8d095edfd6f0e8030f4335
falba3 <99144557+falba3@users.noreply.github.com>
2023-10-15 22:12:42 +0200

    8. testing all

--- auto_mpi.py
+++ auto_mpi.py
@@ -1,3 +1,35 @@
-from auto_inpainter import inpainter
-from auto_masker import auto_masker
+from auto_masker import masker
 from auto_prompter import prompter
+from auto_inpainter import inpainter
+
+
+import os
+import sys
+import logging
+
+
+def main(images_directory):
+    try:
+        if os.path.exists(images_directory):
+            os.chdir(images_directory)
+
+            masker(images_directory)
+            prompter(images_directory)
+            inpainter(images_directory)
+
+        else:
+            os.chdir(images_directory)
+            logging.basicConfig(filename='error.log', level=logging.ERROR)
+            raise Exception(f"directory '{images_directory}' does not exist")
+
+    except Exception as e:
+        os.chdir(images_directory)
+        logging.basicConfig(filename='error.log', level=logging.ERROR)
+        logging.error(f"An error occurred: {str(e)}")
+
+
+
+
+if __name__ == "__main__":
+    inp = "".join(sys.argv[1:])
+    main(inp)
